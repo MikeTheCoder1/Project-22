0459b732377fdd84c22ea9472c73e43edd5ac9a2
MikeTheCoder1 <72530946+MikeTheCoder1@users.noreply.github.com>
2020-11-12 18:11:12 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -27,7 +27,19 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-	boxBottom=createSprite(helicopterSprite.x,helicopterSprite.y,20,200);
+	boxPosition=width/2-100 ,boxY=610; 
+	boxleftSprite=createSprite(boxPosition, boxY, 20,100); 
+	boxleftSprite.shapeColor=color(255,0,0);
+	Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+	World.add(world, boxLeftBody); 
+	boxBase=createSprite(boxPosition+100, boxY+40, 200,20); 
+	boxBase.shapeColor=color(255,0,0); 
+	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} ); 
+	World.add(world, boxBottomBody); 
+	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100); 
+	boxleftSprite.shapeColor=color(255,0,0); 
+	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} ); 
+	World.add(world, boxRightBody);  
 
 
 	engine = Engine.create();
